ea5b47ade47fe754e285d2896fda293a97c3b89f
Dirko Coetsee <dirko@ml.sun.ac.za>
2014-03-03 21:54:00 +0200

    Added grid test for graph generation

--- infer.py
+++ infer.py
@@ -6,7 +6,7 @@ class Model:
         self.factors = []
         self.cardinalities = dict()
         self.variables_to_factors = dict()
-        self.edges = []
+        self.edges = set()
 
         for factor in factor_list:
             self.add_factor(factor)
@@ -26,7 +26,21 @@ class Model:
                 self.variables_to_factors[variable[0]] = {factor}
 
     def build_graph(self):
-        # Build rest of graph by greedily adding the largest sepset factors to the above added node
+
+        def add_edge_to_set(edge, set_to_add):
+            # Edge should be undirected - but we have to use tuples (need to be hashable)
+            if (edge[1], edge[0]) not in set_to_add:
+                set_to_add.add(edge)
+
+        def mark_factors_in_edge(edge, marked_set, unmarked_set):
+            marked_set.add(edge[0])
+            marked_set.add(edge[1])
+            if edge[0] in unmarked_set:
+                unmarked_set.remove(edge[0])
+            if edge[1] in unmarked_set:
+                unmarked_set.remove(edge[1])
+
+        # Build graph by greedily adding the largest sepset factors to the above added node
         for variable, factors in self.variables_to_factors.items():
             marked_factors = set()
             unmarked_factors = set(factors)
@@ -36,33 +50,25 @@ class Model:
                                                                           list(factors),
                                                                           unmarked_factors,  # just for start
                                                                           unmarked_factors)
-                self.edges += [(first_candidate_sepset[0], first_candidate_sepset[1])]
-                marked_factors.add(first_candidate_sepset[0])
-                marked_factors.add(first_candidate_sepset[1])
-                unmarked_factors.remove(first_candidate_sepset[0])
-                unmarked_factors.remove(first_candidate_sepset[1])
+                add_edge_to_set(first_candidate_sepset, self.edges)
+                mark_factors_in_edge(first_candidate_sepset, marked_factors, unmarked_factors)
 
                 while len(marked_factors) < len(factors):
                     largest_sepset = self.get_largest_unmarked_sepset(variable, list(factors), marked_factors,
                                                                       unmarked_factors)
                     # Add largest sepset factors to graph
-                    if largest_sepset[0] is not None:
-                        self.edges += [(largest_sepset[0], largest_sepset[1])]
-                        marked_factors.add(largest_sepset[0])
-                        marked_factors.add(largest_sepset[1])
-                        if largest_sepset[0] in unmarked_factors:
-                            unmarked_factors.remove(largest_sepset[0])
-                        if largest_sepset[1] in unmarked_factors:
-                            unmarked_factors.remove(largest_sepset[1])
+                    if largest_sepset is not None:
+                        add_edge_to_set(largest_sepset, self.edges)
+                        mark_factors_in_edge(largest_sepset, marked_factors, unmarked_factors)
 
     def get_largest_unmarked_sepset(self, variable, factors, marked_factors, unmarked_factors):
-        sepset_sizes = [(factor1, factor2, len(factor1.variable_set.intersection(factor2.variable_set)))
+        sepset_sizes = [((factor1, factor2), len(factor1.variable_set.intersection(factor2.variable_set)))
                         for factor1 in factors for factor2 in factors
                         if factor1 in marked_factors and factor2 in unmarked_factors and
                         factor1 != factor2 and
                         variable in factor1.variable_set and variable in factor2.variable_set]
         if len(sepset_sizes) == 0:
-            max_sepset = (None, None, 0)
+            max_sepset = (None, 0)
         else:
-            max_sepset = max(sepset_sizes, key=lambda x: x[2])
-        return max_sepset
+            max_sepset = max(sepset_sizes, key=lambda x: x[1])
+        return max_sepset[0]
