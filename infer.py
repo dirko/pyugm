- Added parameters to factors - Started putting up learning class - Added (incorrect) method to get log-likelihood (should be removed)

infer.py
@@ -13,6 +13,16 @@ class Model:
         for factor in factor_list:
             self.add_factor(factor)
 
+        self.parameters_to_index = {}
+        # Get number of params and map param to position in param vector
+        for factor in self.factors:
+            if factor.parameters is not None:
+                for parameter in factor.parameters.reshape(-1, ):
+                    if isinstance(parameter, str):
+                        self.parameters_to_index[parameter] = 0
+        for index, key in enumerate(sorted(self.parameters_to_index.keys())):
+            self.parameters_to_index[key] = index
+
     def add_factor(self, factor):
         self.factors.append(factor)
 
@@ -63,7 +73,8 @@ class Model:
                         add_edge_to_set(largest_sepset, self.edges)
                         mark_factors_in_edge(largest_sepset, marked_factors, unmarked_factors)
 
-    def get_largest_unmarked_sepset(self, variable, factors, marked_factors, unmarked_factors):
+    @staticmethod
+    def get_largest_unmarked_sepset(variable, factors, marked_factors, unmarked_factors):
         sepset_sizes = [((factor1, factor2), len(factor1.variable_set.intersection(factor2.variable_set)))
                         for factor1 in factors for factor2 in factors
                         if factor1 in marked_factors and factor2 in unmarked_factors and
@@ -77,7 +88,7 @@ class Model:
 
     def set_evidence(self, evidence, normalize=False):
         """ param evidence: list of (variable name, value) pairs """
-        for variable, value in evidence:
+        for variable, value in evidence.items():
             for factor in self.variables_to_factors[variable]:
                 factor.set_evidence([(variable, value)], normalize=normalize, inplace=True)
 
@@ -86,6 +97,30 @@ class Model:
         Return marginals of all the factors in which the variable appears.
         """
         return [factor.marginalize([variable], normalize=normalize) for factor in self.variables_to_factors[variable]]
+
+    def set_parameters(self, parameters):
+        """
+        Iterate through factors and fill factor potentials with these new parameters.
+        """
+        for factor in self.factors:
+            original_shape = factor.data.shape
+            new_data = factor.data.reshape(-1, )
+            for i, parameter in enumerate(factor.parameters.reshape(-1, )):
+                if isinstance(parameter, str):
+                    new_data[i] = parameters[self.parameters_to_index[parameter]]
+                else:
+                    new_data[i] = parameter
+            factor.data = new_data.reshape(original_shape)
+
+    def get_log_likelihood(self, evidence):
+        """ Return the log likelihood of the evidence. Must be called on calibrated model. """
+        log_likelihood = 0.0
+        for factor in self.factors:
+            print 'log_lik dat', factor, factor.data
+            print 'log_lik', factor, factor.get_potential(evidence.items()), factor.data.sum()
+            print 'log_marg', factor.marginalize(evidence.keys()).data
+            log_likelihood += np.log(factor.get_potential(evidence.items()).sum()) - np.log(factor.data.sum())
+        return log_likelihood
 
 
 class LoopyBeliefUpdateInference:

learn.py
@@ -3,7 +3,30 @@ from infer import Model, LoopyBeliefUpdateInference
 import numpy as np
 
 
-class LearnMRFParameters():
+class LearnMRFParameters:
     """
     Find an approximation to the posterior given a model and prior.
     """
+    def __init__(self, model, evidence, prior=1.0):
+        self.model = model
+        self.evidence = evidence
+        self.parameters = np.zeros(len(self.model.parameters_to_index))
+
+    def evaluate_objective_derivative(self):
+        #self.model.set_parameters(self.parameters)
+        #self.model.set_evidence(evidence=self.evidence)
+        #inference = LoopyBeliefUpdateInference(self.model)
+        #inference.set_up_belief_update()
+        #change = inference.update_beliefs(number_of_updates=35)
+        #Z_observed = self.model.factors[0].data.sum()
+        #empirical_expected_features = self.model.get_expected_parameters()
+
+        self.model.set_parameters(self.parameters)
+        inference = LoopyBeliefUpdateInference(self.model)
+        inference.set_up_belief_update()
+        change = inference.update_beliefs(number_of_updates=35)
+        Z_total = self.model.factors[0].data.sum()
+        #model_expected_features = self.model.get_expected_parameters()
+        log_likelihood = np.log(Z_observed) - np.log(Z_total)
+
+        #gradient = empirical_expected_features - model_expected_features

test_infer.py
@@ -5,6 +5,7 @@ import numpy as np
 from factor import DiscreteFactor
 from infer import Model, LoopyBeliefUpdateInference
 
+
 def print_edge_set(edges):
     for edge in list(edges):
         print '({0}, {1})'.format(edge[0], edge[1])
@@ -124,11 +125,25 @@ class TestModel(unittest.TestCase):
         a = DiscreteFactor([1, 2, 3], np.array(range(0, 8)).reshape((2, 2, 2)))
         b = DiscreteFactor([2, 3, 4], np.array(range(1, 9)).reshape((2, 2, 2)))
         model = Model([a, b])
-        evidence = [(2, 1), (4, 0)]
+        evidence = {2: 1, 4: 0}
         model.set_evidence(evidence)
 
         c = DiscreteFactor([1, 2, 3], np.array(range(0, 8)).reshape((2, 2, 2))).set_evidence(evidence)
         d = DiscreteFactor([2, 3, 4], np.array(range(1, 9)).reshape((2, 2, 2))).set_evidence(evidence)
+
+        assert_array_almost_equal(c.data, model.factors[0].data)
+        assert_array_almost_equal(d.data, model.factors[1].data)
+
+    def test_set_parameters(self):
+        a = DiscreteFactor([1, 2], parameters=np.array([[1, 2], ['a', 0.0]], dtype=object))
+        b = DiscreteFactor([2, 3], parameters=np.array([['b', 'c'], ['d', 'a']]))
+        model = Model([a, b])
+        print a.parameters
+        new_parameters = np.array([1, 2, 3, 4])
+        model.set_parameters(new_parameters)
+
+        c = DiscreteFactor([1, 2], np.array([1, 2, 1, 0]).reshape((2, 2)))
+        d = DiscreteFactor([2, 3], np.array([2, 3, 4, 1]).reshape((2, 2)))
 
         assert_array_almost_equal(c.data, model.factors[0].data)
         assert_array_almost_equal(d.data, model.factors[1].data)
@@ -284,6 +299,45 @@ class TestLoopyBeliefUpdateInference(unittest.TestCase):
         self.assertEqual(d.variables, c.variables)
         self.assertEqual(d.axis_to_variable, c.axis_to_variable)
         assert_array_almost_equal(d.data, c.data)
+
+    def test_get_log_likelihood(self):
+        a = DiscreteFactor([1, 2], data=np.array([[1, 2.0], [3, 4]]))
+        b = DiscreteFactor([2, 3], data=np.array([[3, 4.0], [5, 7]]))
+        # 1 2 3  |
+        # -------+----------
+        # 0 0 0  | 1 * 3 = 3
+        # 0 0 1  | 1 * 4 = 4
+        # 0 1 0  | 2 * 5 = 10
+        # 0 1 1  | 2 * 7 = 14
+        # 1 0 0  | 3 * 3 = 9
+        # 1 0 1  | 3 * 4 = 12
+        # 1 1 0  | 4 * 5 = 20
+        # 1 1 1  | 4 * 7 = 28
+        #
+        # p(1=0, 2, 3=1) = [4, 14] / 100
+        # => p(1=0, 3=1) = 18 / 100
+
+        model = Model([a, b])
+        evidence = {1: 0, 3: 1}
+        model.build_graph()
+        inference = LoopyBeliefUpdateInference(model)
+
+        c = inference.exhaustive_enumeration()
+        print c
+        print c.data
+        print c.get_potential(evidence.items())
+
+        model.set_evidence(evidence)
+        inference = LoopyBeliefUpdateInference(model)
+        inference.set_up_belief_update()
+
+        change = inference.update_beliefs(number_of_updates=35)
+        print 'Last iteration: ', change
+
+        actual_log_likelihood = model.get_log_likelihood(evidence=evidence)
+        print actual_log_likelihood, np.log(0.18)
+        self.assertAlmostEqual(actual_log_likelihood, np.log(0.18))
+
 
 if __name__ == '__main__':
     unittest.main()