5df9c2dc539374d68335a616a0ba5f6897b6e106
Dirko Coetsee <dirko@ml.sun.ac.za>
2014-11-03 22:45:49 +0200

    Trying to figure out what is going on with the example.

--- examples/example_hmm.py
+++ examples/example_hmm.py
@@ -2,53 +2,8 @@
 import numpy as np
 
 from pyugm.factor import DiscreteFactor
-from pyugm.infer import LoopyBeliefUpdateInference
 from pyugm.model import Model
 from pyugm.learn import LearnMRFParameters
-
-
-def example_fully_specified_run():
-    seq = [1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0]
-    emissions = np.zeros((2, 2))
-    emissions[0, 0] = 0.8
-    emissions[0, 1] = 0.2
-    emissions[1, 0] = 0.3
-    emissions[1, 1] = 0.7
-
-    transitions = np.zeros((2, 2))
-    transitions[0, 0] = 0.9
-    transitions[0, 1] = 0.1
-    transitions[1, 0] = 0.2
-    transitions[1, 1] = 0.8
-
-    a = DiscreteFactor([0], np.array([0.5, 0.5]))
-    factors = [a]
-    for i, s in enumerate(seq):
-        a = DiscreteFactor([i, i + 100], np.array(emissions))
-        a.set_evidence([(i + 100, s)], normalize=True, inplace=True)
-        factors.append(a)
-
-        if i != 0:
-            b = DiscreteFactor([i - 1, i], np.array(transitions))
-            factors.append(b)
-
-    model = Model(factors)
-
-    for factor in model.factors:
-        print factor, factor.data
-
-    inference = LoopyBeliefUpdateInference(model)
-
-    change = inference.calibrate(number_of_updates=185)
-    print change
-    for factor in model.factors:
-        print factor, factor.data
-
-    for i in xrange(0, len(seq)):
-        for factor in model.get_marginals(i, normalize=False):
-            print i, factor.data, factor.data.sum()
-    for i, s in enumerate(seq):
-        print i, s, model.get_marginals(i)[0].data
 
 
 def example_learning_run():
@@ -63,14 +18,14 @@ def example_learning_run():
     for s, seq in enumerate(seqs):
         for e, elem in enumerate(seq):
             factors.append(DiscreteFactor([e + 100 * s, e + 1 + 100 * s],
-                                          parameters=np.array([['a', 'b'], ['c', 'd']])))
+                                          parameters=np.array([['trans00', 'trans01'], ['trans10', 'trans11']])))
             factors.append(DiscreteFactor([e + 100 * s, e + 100 * s + 10000],
-                                          parameters=np.array([['e', 'f'], ['g', 'h']])))
+                                          parameters=np.array([['obs00', 'obs01'], ['obs10', 'obs11']])))
             evidence[e + 100 * s + 10000] = elem
     model = Model(factors)
     learner = LearnMRFParameters(model, prior=1.0)
     learner.fit(evidence)
-    print learner.ans
+    print learner.parameters
 
 if __name__ == '__main__':
     example_learning_run()
